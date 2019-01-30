a51d7265226a88d2fa18dc1bd9484fe4ff12e822
Vanessa Frenkel <vanefrenkel@gmail.com>
2019-01-30 20:48:05 -0300

    changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    def check_if_completed(obj)
+        if obj.completed == true
+            'si'
+        else
+            'no'
+        end
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   get   'todos/list', to: 'todos#list'
-  root  'todos#index'
-  get   'todos',  to: 'todos#index'
-  get   'todos/new'
-  post  'todos',  to: 'todos#create'
-  get   'todos/:id', to: 'todos#show', as: 'todo'
   get   'todos/:id/edit', to: 'todos#edit' , as: 'todos_edit'
+  get    'todos/:id/complete', to: 'todos#complete', as: 'todos_completed'
+  get   'todos/new'
+  
+  post  'todos',  to: 'todos#create'
   patch  'todos/:id', to: 'todos#update'
   delete 'todos/:id', to: 'todos#destroy'
-  get    'todos/:id/complete', to: 'todos#complete', as: 'todos_completed'
+  root  'todos#index'
+  get   'todos/:id', to: 'todos#show', as: 'todo'
+  get   'todos',  to: 'todos#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
