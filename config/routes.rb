df32f4409e092d28e7711c1fd8e556dff4f5dcb2
Vanessa Frenkel <vanefrenkel@gmail.com>
2019-01-30 19:17:02 -0300

    optimization

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get    'todos/list', to: 'todos#list'
+  get   'todos/list', to: 'todos#list'
   root  'todos#index'
   get   'todos',  to: 'todos#index'
   get   'todos/new'
   post  'todos',  to: 'todos#create'
   get   'todos/:id', to: 'todos#show', as: 'todo'
-  get   'todos/:id/edit', to: 'todos#edit'
+  get   'todos/:id/edit', to: 'todos#edit' , as: 'todos_edit'
   patch  'todos/:id', to: 'todos#update'
   delete 'todos/:id', to: 'todos#destroy'
-  get    'todos/:id/complete', to: 'todos#complete'
+  get    'todos/:id/complete', to: 'todos#complete', as: 'todos_completed'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
