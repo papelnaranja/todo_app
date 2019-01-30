62cff88bb422d624eba3228ff65fd73c0f3f8562
Vanessa Frenkel <vanefrenkel@gmail.com>
2019-01-29 23:00:56 -0300

    added new and create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,17 @@ class TodosController < ApplicationController
     def index   
         @todos = Todo.all
     end
+    def new
+        @todo = Todo.new
+    end
+    def create
+        @todo = Todo.new(todo_params)
+        @post.save
+        redirect todos_path
+    end
+
+    private
+    def todo_params
+        params.require(:todo).permit(:description,:completed)
+    end
 end
