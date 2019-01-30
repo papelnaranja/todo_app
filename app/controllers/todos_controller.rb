616a8cc261a2216bdee909bb863cddea810409a3
Vanessa Frenkel <vanefrenkel@gmail.com>
2019-01-30 00:02:40 -0300

    added show edit update and destroy

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,10 +7,27 @@ class TodosController < ApplicationController
     end
     def create
         @todo = Todo.new(todo_params)
-        @post.save
-        redirect todos_path
+        @todo.save
+        
+        redirect_to todos_path
     end
+    def show
+        @todo = Todo.find(params[:id])
+    end
+    def edit
+        @todo = Todo.find(params[:id])
+    end
+    def update
+        @todo = Todo.find(params[:id])
+        @todo.update(todo_params)
+        redirect_to ptodos_path
+    end
+    def destroy
+        @todo = Todo.find(params[:id])
+        @todo.destroy
+        redirect_to todos_path
 
+    end
     private
     def todo_params
         params.require(:todo).permit(:description,:completed)
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get   'todos',  to: 'todos#index'
   get   'todos/new'
   post  'todos',  to: 'todos#create'
+  get   'todos/:id', to: 'todos#show', as: 'todo'
+  get   'todos/:id/edit', to: 'todos#edit'
+  patch  'todos/:id', to: 'todo#update'
+  delete  'todos/:id', to: 'todo#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
